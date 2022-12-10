e1aaf4afa6095cee8010dbbec132059e07d2c066
Remy Wood <99709675+remwood@users.noreply.github.com>
2022-12-09 18:19:02 -0800

    Add navigation setup

--- App.js
+++ App.js
@@ -1,7 +1,20 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { SafeAreaView, StyleSheet } from "react-native";
+import TodosScreen from "./components/TodosScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return <SafeAreaView style={styles.container}></SafeAreaView>;
+  return (
+    <SafeAreaView style={styles.container}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Todos" component={TodosScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
---
+++ components/TodosScreen.jsx
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+
+export default function TodosScreen() {
+  return <Text>TODO</Text>;
+}
